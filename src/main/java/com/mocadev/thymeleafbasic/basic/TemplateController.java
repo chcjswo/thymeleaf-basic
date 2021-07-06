150a90321d8ea28b0f8ee836bb4ddce9b3abfa92
tony <chcjswo@gmail.com>
2021-07-07 00:41:01 +0900

    템플릿 조각

---
+++ src/main/java/com/mocadev/thymeleafbasic/basic/TemplateController.java
@@ -0,0 +1,23 @@
+package com.mocadev.thymeleafbasic.basic;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ * @author chcjswo
+ * @version 1.0.0
+ * @blog https://mocadev.tistory.com
+ * @github https://github.com/chcjswo
+ * @since 2021-07-07
+ **/
+@Controller
+@RequestMapping("/template")
+public class TemplateController {
+
+	@GetMapping("/fragment")
+	public String template() {
+		return "template/fragment/fragmentMain";
+	}
+
+}
